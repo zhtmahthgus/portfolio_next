dadae287ec3d67259d91e85ea1f72394e39da0ab
zhtmahthgus <zhtmahthgus@naver.com>
2022-09-22 16:04:26 +0900

    deploy test

--- pages/posts/activity1.js
+++ pages/posts/activity1.js
@@ -1,5 +1,5 @@
 import styles from "../../styles/Posts.module.scss";
 
 export default function Activity1() {
-  return <div className={styles.container}>activity1</div>;
+  return <div className={styles.container}>activity</div>;
 }
