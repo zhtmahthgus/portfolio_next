fb0f24f7ffa1375e9b44213b066d319ce3edf81c
zhtmahthgus <zhtmahthgus@naver.com>
2022-09-23 16:44:11 +0900

    add video

--- pages/posts/activity1.js
+++ pages/posts/activity1.js
@@ -1,5 +1,17 @@
 import styles from "../../styles/Posts.module.scss";
 
 export default function Activity1() {
-  return <div className={styles.container}>activity</div>;
+  return (
+    <div className={styles.container}>
+      <iframe
+        width="600"
+        height="360"
+        src="https://www.youtube.com/embed/pKe66hwKAtQ"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
 }
